63f8eae807c535a1e413236de76a8d17231a9b7d
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-13 16:21:09 +0800

    Initialized React project for WeatherPulse.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>WeatherPulse</h1>
+        <h6>Powered by OpenWeather</h6>
+      </header>
+
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
