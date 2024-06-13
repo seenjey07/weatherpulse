4ec1b91d438a519b79969b2451c981139ed552e4
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-13 17:48:41 +0800

    Initialized components.

---
+++ src/components/OtherInformation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OtherInformation = () => {
+  return <div>OtherInformation</div>;
+};
+
+export default OtherInformation;
---
+++ src/components/WeeklyForecast.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeeklyForecast = () => {
+  return <div>WeeklyForecast</div>;
+};
+
+export default WeeklyForecast;
