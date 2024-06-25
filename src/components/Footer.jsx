e90b8497ec6f7856e6ed9c66363333d70af00f80
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-25 16:55:11 +0800

    Added styles.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <p>Powered by OpenWeather</p>
-      <p>&copy; {new Date().getFullYear()} WeatherPulse</p>
+    <div className="bg-white/10 p3 md:px-8 md:py-2 flex flex-row justify-evenly align-middle font-mono">
+      <p className="text-xs text-gray-200">
+        &copy; {new Date().getFullYear()} WeatherPulse
+      </p>
+      <p className="text-xs text-gray-200">Powered by OpenWeather API</p>
     </div>
   );
 };
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Loading = () => (
-  <Button disabled>
+  <Button disabled className="w-screen m-10">
     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
     Please wait
   </Button>
