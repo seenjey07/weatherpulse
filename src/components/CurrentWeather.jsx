5b3a62b4d51211dcb38d8e3be1855d742024df10
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-26 17:23:08 +0800

    Removed icons in png in the assets folder. Added capitalizeFirstLetters, and styles.

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -4,13 +4,12 @@ import SearchBar from "./SearchBar";
 import Loading from "./Loading";
 import { Card, CardContent, CardTitle } from "./ui/card";
 
-import clearIcon from "./assets/icons/clear.png";
-import cloudIcon from "./assets/icons/cloud.png";
-import drizzleIcon from "./assets/icons/drizzle.png";
-import rainIcon from "./assets/icons/rain.png";
-import snowIcon from "./assets/icons/snow.png";
-import windIcon from "./assets/icons/wind.png";
-import humidityIcon from "./assets/icons/humidity.png";
+import clearIcon from "./assets/icons/sun.mp4";
+import cloudIcon from "./assets/icons/cloudy.mp4";
+import rainIcon from "./assets/icons/rain.mp4";
+import snowIcon from "./assets/icons/snow.mp4";
+import windyIcon from "./assets/icons/windy.mp4";
+import thunderstormIcon from "./assets/icons/thunderstorm.mp4";
 
 const CurrentWeather = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -77,37 +76,93 @@ const CurrentWeather = () => {
       .replace(" ", "")}`;
   };
 
+  const capitalizeFirstLetters = (str) => {
+    return str.replace(/\b\w/g, (char) => char.toUpperCase());
+  };
+
   return (
     <>
       <SearchBar onSearch={handleSearch} />
 
-      <Card className="m-auto p-2 font-mono bg-amber-300">
+      <Card className="m-auto mt-4 p-2 font-mono bg-amber-300 flex-1 place-content-center">
         {!weatherData && <Loading />}
 
         {weatherData && (
           <CardContent className="text-center py-3 gap-2">
-            <CardTitle className="text-3xl text-amber-800">
-              {weatherData.main?.temp}°C
-            </CardTitle>
-            {weatherData.weather?.[0]?.main.includes("clear") ? (
-              <img
+            {weatherData.weather?.[0]?.main === "Clear" ||
+            weatherData.weather?.[0]?.main === "Sun" ? (
+              <video
                 src={clearIcon}
                 alt="clearWeatherIcon"
                 className="weatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
               />
-            ) : weatherData.weather?.[0]?.main.includes("cloud") ? (
-              <img src={cloudIcon} alt="cloudyWeatherIcon" />
-            ) : weatherData.weather?.[0]?.main.includes("drizzle") ? (
-              <img src={drizzleIcon} alt="drizzleWeatherIcon" />
-            ) : weatherData.weather?.[0]?.main.includes("rain") ? (
-              <img src={rainIcon} alt="rainWeatherIcon" />
-            ) : weatherData.weather?.[0]?.main.includes("snow") ? (
-              <img src={snowIcon} alt="snowWeatherIcon" />
-            ) : weatherData.weather?.[0]?.main.includes("wind") ? (
-              <img src={windIcon} alt="windWeatherIcon" />
+            ) : weatherData.weather?.[0]?.main === "Clouds" ? (
+              <video
+                className="weatherIcon"
+                src={cloudIcon}
+                alt="cloudyWeatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
+              />
+            ) : weatherData.weather?.[0]?.main === "Rain" ? (
+              <video
+                className="weatherIcon"
+                src={rainIcon}
+                alt="rainWeatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
+              />
+            ) : weatherData.weather?.[0]?.main === "Snow" ? (
+              <video
+                className="weatherIcon"
+                src={snowIcon}
+                alt="snowWeatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
+              />
+            ) : weatherData.weather?.[0]?.main === "Windy" ? (
+              <video
+                className="weatherIcon"
+                src={windyIcon}
+                alt="windWeatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
+              />
+            ) : weatherData.weather?.[0]?.main === "Thunderstorm" ? (
+              <video
+                className="weatherIcon"
+                src={thunderstormIcon}
+                alt="thunderstormWeatherIcon"
+                type="video/mp4"
+                autoPlay={true}
+                loop={true}
+                muted={true}
+                controls={false}
+              />
             ) : (
               <p>Condition: {weatherData.weather?.[0]?.main}</p>
             )}
+
+            <CardTitle className="text-3xl text-amber-800">
+              {Math.round(weatherData.main?.temp)}°C
+            </CardTitle>
             <CardTitle className="pt-2">
               {weatherData.name}, {weatherData.sys?.country}
             </CardTitle>
@@ -116,26 +171,41 @@ const CurrentWeather = () => {
         )}
       </Card>
 
-      <Card className="m-auto p-2 font-mono bg-white">
-        <CardTitle className="text-xl text-amber-800">
-          Weather Details
-        </CardTitle>
+      <div className="m-5">
+        <Card className="m-auto mt-3 font-mono bg-amber-300 w-fit place-content-center">
+          <CardTitle className="text-lg text-amber-800 pl-2 pt-1">
+            Weather Details
+          </CardTitle>
 
-        {weatherData && (
-          <div className="m-auto p-2 font-mono">
-            <img src={windIcon} alt="windIcon" className="weatherIcon" />
-            <p>{weatherData.wind?.speed} m/s</p>
-            <img
-              src={humidityIcon}
-              alt="humidityIcon"
-              className="weatherIcon"
-            />
-            <p>{weatherData.main?.humidity}%</p>
-            <p>Sunrise: {formatDateTime(weatherData.sys?.sunrise)}</p>
-            <p>Sunset: {formatDateTime(weatherData.sys?.sunset)}</p>
-          </div>
-        )}
-      </Card>
+          {weatherData && (
+            <div className="m-auto py-1 px-3 font-mono flex flex-col items-center justify-center">
+              <div className="flex flex-row flex-nowrap m-auto text-center py-2">
+                <p className="flex flex-row flex-wrap justify-center">
+                  <b>Condition:</b>
+                  {capitalizeFirstLetters(
+                    weatherData.weather?.[0]?.description
+                  )}
+                </p>
+                <p className="px-3">
+                  <b>Wind Speed:</b> {weatherData.wind?.speed}m/s
+                </p>
+                <p>
+                  <b>Humidity:</b> {weatherData.main?.humidity}%
+                </p>
+              </div>
+              <hr className="w-full" />
+              <div className="text-center pt-2">
+                <p>
+                  <b>Sunrise:</b> {formatDateTime(weatherData.sys?.sunrise)}
+                </p>
+                <p>
+                  <b>Sunset:</b> {formatDateTime(weatherData.sys?.sunset)}
+                </p>
+              </div>
+            </div>
+          )}
+        </Card>
+      </div>
     </>
   );
 };
