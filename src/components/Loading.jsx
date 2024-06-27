5f8252dd7394671fdd24888401f8617fe87a7155
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-06-27 15:09:24 +0800

    Changed m to auto.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Loading = () => (
-  <Button disabled className="w-screen m-10">
+  <Button disabled className="w-full m-auto">
     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
     Please wait
   </Button>
