4bb9ee1d09201f6dc83cb7392820d39618644f65
ChristineTauy <chrosales07@gmail.com>
2025-11-18 17:21:30 +0800

    Style(css): Modify loading text and adjust color

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,9 +2,9 @@ import { Loader2 } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Loading = () => (
-  <Button className="w-full m-auto bg-transparent font-mono text-gray-600">
+  <Button className="w-full m-auto bg-transparent text-secondary text-md">
     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-    Please wait
+    Please wait a moment...
   </Button>
 );
 
